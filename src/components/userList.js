0e149be1c66e4a85d5cc6c2e48ab7ff81876ec58
geoffrey1330 <israelgeoffrey13@gmail.com>
2022-02-03 01:20:55 +0100

    first commit

--- src/components/userList.js
+++ src/components/userList.js
@@ -55,32 +55,7 @@ const UserList = ({users, openAdd,openDelete}) => {
         setSortedUsers(sortedUsers);
     }
 
-    // const sortUsers = () => {
-    //     let sorted = users.slice();
-    //         sorted.sort((x,y) =>
-    //         {
-    //             let UserX = x.username.toLowerCase();
-    //             let UserY = y.username.toLowerCase();
-        
-    //             return (UserX > UserY) ? -1 : (UserX < UserY) ? 1 : 0;
-    //         })
-        
-       
-    //     const sortedUsers = sorted.map(user => {
-    //         return (
-    //             <tr key={user.id}>
-    //                 <td>{user.id}</td>
-    //                 <td>{user.name}</td>
-    //                 <td>{user.username}</td>
-    //                 <td>{user.email}</td>
-    //                 <td>{user.address? user.address.city : ''}</td>
-    //                 <td><button type="button" className="btn btn-warning td-btn" onClick={() => openAdd(user.id)}>Edit</button></td>
-    //                 <td><button type="button" className="btn btn-danger td-btn" onClick={() => openDelete(user.id)}>Delete</button></td>
-    //             </tr>
-    //         )
-    //     })
-    //     setSortedUsers(sortedUsers);
-    // }
+    
     return (
         <div className="card">
         <div className="add-row">
