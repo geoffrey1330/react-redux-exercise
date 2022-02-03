94d6c0695bf50e80eaf8c69d53895dc9f51c6565
geoffrey1330 <israelgeoffrey13@gmail.com>
2022-02-03 01:18:06 +0100

    first commit

--- src/store.js
+++ src/store.js
@@ -5,28 +5,6 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/reducer';
 
 const middleware = [thunk]
-const initialState = {}
-// const store = createStore(reducer, initialState,
-//     composeWithDevTools(applyMiddleware(...middleware)))
-
-//import stor from './store'
-
-const logger = store => {
-  return next =>{
-      return action =>{
-          console.log('[Middleware] Dispatching', action);
-          const result = next(action);
-          //console.log('[Middleware] next state', stor.getState());
-          return result;
-      }
-  }
-};
-
-const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-//const store = createStore(reducer, composeEnchancers(applyMiddleware(...middleware)));
-
-//export default store
 
 
 const store = createStore(
